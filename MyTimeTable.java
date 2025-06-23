d0b82a0437da92d147cb9c0ccde087b6b0a6f7c6
Anna-nge <nangnunuaye39@gmail.com>
2025-06-23 13:08:36 +0700

    Add files via upload

---
+++ MyTimeTable.java
@@ -0,0 +1,29 @@
+/*
+ * Name: Nang Nu Nu Aye
+ * ID:6643005
+ * Section-545
+ */
+package worksheet1;
+
+public class MyTimeTable {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Nang Nu Nu Aye");
+		System.out.println("ID: 6643005");
+		System.out.println("Nickname: Anna");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+ Date_Time |9:00-10:30|10:30-12:00|12:00-13:30|13:30-15:00|15:00-16:30+");
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+ Monday    |GE1411(401)|          |ELE2001(644)|      ITX2007(542)    |");
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+ Tuesday   |     CSX3002(545)     |ELE2001(644)|                      |");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+ Wednesday |GE1411(401)|          |ELE2001(644)|     BG14032(430)     |");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+ Thursday  |           |          |            |            |         |");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+ Friday    |        ITX2005(545)  |ELE2001(644)|      CSX2008(543)    |");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+	}
+
+}
